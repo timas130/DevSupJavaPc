c740f5da51ae471e97e074e12a8f25ee324e48ca
ZeonXX <zeooondevel@gmail.com>
2018-05-18 00:11:07 +0700

    Commit

--- src/main/java/com/sup/dev/java_pc/yandex/YandexTranslate.java
+++ src/main/java/com/sup/dev/java_pc/yandex/YandexTranslate.java
@@ -16,22 +16,20 @@ public class YandexTranslate {
 
     public String translate(String targetLang, String input) throws IOException {
 
-        return input;
+        String urlStr = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20150627T071448Z.117dacaac1e63b79.6b1b4bb84635161fcd400dace9fb2220d6f344ef";
+        URL urlObj = new URL(urlStr);
+        HttpsURLConnection connection = (HttpsURLConnection) urlObj.openConnection();
+        connection.setRequestMethod("POST");
+        connection.setDoOutput(true);
+        DataOutputStream dataOutputStream = new DataOutputStream(connection.getOutputStream());
+        dataOutputStream.writeBytes("text=" + URLEncoder.encode(input, "UTF-8") + "&lang=" + targetLang);
 
-     // String urlStr = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20150627T071448Z.117dacaac1e63b79.6b1b4bb84635161fcd400dace9fb2220d6f344ef";
-     // URL urlObj = new URL(urlStr);
-     // HttpsURLConnection connection = (HttpsURLConnection) urlObj.openConnection();
-     // connection.setRequestMethod("POST");
-     // connection.setDoOutput(true);
-     // DataOutputStream dataOutputStream = new DataOutputStream(connection.getOutputStream());
-     // dataOutputStream.writeBytes("text=" + URLEncoder.encode(input, "UTF-8") + "&lang=" + targetLang);
-//
-     // InputStream response = connection.getInputStream();
-     // String json = new java.util.Scanner(response).nextLine();
-     // int start = json.indexOf("[");
-     // int end = json.indexOf("]");
-//
-     // return json.substring(start + 2, end - 1);
+        InputStream response = connection.getInputStream();
+        String json = new java.util.Scanner(response).nextLine();
+        int start = json.indexOf("[");
+        int end = json.indexOf("]");
+
+        return json.substring(start + 2, end - 1);
 
     }
 
