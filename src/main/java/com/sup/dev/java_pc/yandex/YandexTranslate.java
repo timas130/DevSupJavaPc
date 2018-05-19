ff43247bd3b256c952f56a52da5b07c48ae18a6b
ZeonXX <zeooondevel@gmail.com>
2018-05-20 00:07:13 +0700

    Commit

--- src/main/java/com/sup/dev/java_pc/yandex/YandexTranslate.java
+++ src/main/java/com/sup/dev/java_pc/yandex/YandexTranslate.java
@@ -1,8 +1,12 @@
 package com.sup.dev.java_pc.yandex;
 
+import com.sup.dev.java.libs.debug.Debug;
+
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLEncoder;
 
@@ -15,8 +19,8 @@ public class YandexTranslate {
     }
 
     public String translate(String targetLang, String input) throws IOException {
-
-        String urlStr = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20150627T071448Z.117dacaac1e63b79.6b1b4bb84635161fcd400dace9fb2220d6f344ef";
+        Debug.log(targetLang, input);
+        String urlStr = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20180417T025628Z.c947d37bbba4cd90.5441a8db317dd6d5dd48210958933cda684e01ea";
         URL urlObj = new URL(urlStr);
         HttpsURLConnection connection = (HttpsURLConnection) urlObj.openConnection();
         connection.setRequestMethod("POST");
@@ -24,12 +28,18 @@ public class YandexTranslate {
         DataOutputStream dataOutputStream = new DataOutputStream(connection.getOutputStream());
         dataOutputStream.writeBytes("text=" + URLEncoder.encode(input, "UTF-8") + "&lang=" + targetLang);
 
-        InputStream response = connection.getInputStream();
-        String json = new java.util.Scanner(response).nextLine();
-        int start = json.indexOf("[");
-        int end = json.indexOf("]");
+        try {
+            InputStream response = connection.getInputStream();
+            String json = new java.util.Scanner(response).nextLine();
+            int start = json.indexOf("[");
+            int end = json.indexOf("]");
+            return json.substring(start + 2, end - 1);
+        }catch (IOException e){
+            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+            while (br.ready()) Debug.log(br.readLine());
+            throw e;
+        }
 
-        return json.substring(start + 2, end - 1);
 
     }
 
