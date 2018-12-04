e3b0510e2f6720647e1236efda9955da2d3946cb
zeooondevel <zeooondevel@gmail.com>
2018-12-04 21:07:15 +0700

    Commit

--- src/main/java/com/sup/dev/java_pc/google/GoogleNotification.kt
+++ src/main/java/com/sup/dev/java_pc/google/GoogleNotification.kt
@@ -1,9 +1,10 @@
 package com.sup.dev.java_pc.google
 
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential
+import com.sup.dev.java.libs.debug.Debug
 import com.sup.dev.java.libs.debug.info
 import com.sup.dev.java.libs.debug.log
 import com.sup.dev.java.libs.json.Json
+import com.sup.dev.java.libs.json.JsonArray
 import java.io.*
 import java.net.HttpURLConnection
 import java.net.URL
@@ -14,47 +15,30 @@ import java.util.concurrent.TimeUnit
 
 object GoogleNotification {
 
-    private val threadPool: ThreadPoolExecutor
+    private val threadPool: ThreadPoolExecutor = ThreadPoolExecutor(1, 1, 1, TimeUnit.MINUTES, LinkedBlockingQueue())
 
     private var onTokenNotFound: ((String) -> Unit)? = null
-    private var urlKey: String? = null
-    private var filePostfix: String = ""
+    private var apiKey: String? = null
 
-    init {
-        threadPool = ThreadPoolExecutor(1, 1, 1, TimeUnit.MINUTES, LinkedBlockingQueue())
-    }
-
-    fun init(urlKey: String, filePostfix: String = "") {
-        GoogleNotification.urlKey = urlKey
-        GoogleNotification.filePostfix = filePostfix
+    fun init(apiKey: String) {
+        GoogleNotification.apiKey = apiKey
     }
 
     fun send(message: String, vararg tokens: String) {
-        threadPool.execute {
-            for (token in tokens) sendNow(message, token)
-        }
+        threadPool.execute { sendNow(message, *tokens) }
     }
 
-    fun sendNow(message: String, token: String) {
+    fun sendNow(message: String, vararg tokens: String) {
 
         try {
 
             val jsonRoot = Json()
-                    .put("message", Json()
-                            .put("token", token)
-                            .put("android", Json().put("ttl", "20s"))
-                            .put("data", Json().put("my_data", message))
-                            .put("android", Json().put("priority", "high")))
+                    .put("registration_ids", JsonArray().put(*tokens))
+                    .put("data", Json().put("my_data", message))
 
-            val googleCredential = GoogleCredential
-                    .fromStream(FileInputStream(File("service-account$filePostfix.json")))
-                    .createScoped(listOf("https://www.googleapis.com/auth/firebase.messaging"))
-            googleCredential.refreshToken()
-            val accessToken = googleCredential.accessToken
-
-            val url = URL("https://fcm.googleapis.com/v1/projects/$urlKey/messages:send")
+            val url = URL("https://fcm.googleapis.com/fcm/send")
             val conn = url.openConnection() as HttpURLConnection
-            conn.setRequestProperty("Authorization", "Bearer $accessToken")
+            conn.setRequestProperty("Authorization", "key=$apiKey")
             conn.setRequestProperty("Content-Type", "application/json; UTF-8")
             conn.requestMethod = "POST"
             conn.useCaches = false
@@ -80,9 +64,14 @@ object GoogleNotification {
                             onTokenNotFound!!.invoke(j.getString("token")!!)
                         }
                     }
+                } else if (json.containsKey("results")) {
+                    val jsons = json.getJsonArray("results")!!
+                    for (i in 0 until jsons.size()) {
+                        val j = jsons.getJson(i)
+                        if(j.containsKey("error") && j.getString("error", "") == "NotRegistered")
+                            onTokenNotFound!!.invoke(tokens[i])
+                    }
                 }
-            } else if(status == 404) {
-                onTokenNotFound!!.invoke(token)
             } else {
                 info("Google notification sending error. code = $status")
                 val br = BufferedReader(InputStreamReader(conn.errorStream))
