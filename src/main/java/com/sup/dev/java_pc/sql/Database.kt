d0794f140db47849019cd6b3948c4fb71206f8af
Dmitriy Kholin <zeooon@ya.ru>
2021-05-05 11:23:00 +0700

    Commit

--- src/main/java/com/sup/dev/java_pc/sql/Database.kt
+++ src/main/java/com/sup/dev/java_pc/sql/Database.kt
@@ -1,46 +1,11 @@
 package com.sup.dev.java_pc.sql
 
-import com.sup.dev.java.classes.items.Item2
-import com.sup.dev.java.tools.ToolsThreads
-import java.lang.RuntimeException
-
 object Database {
 
-    private var pool = ArrayList<Item2<DatabaseInstance, Boolean>>()
-    private var statisticCollector: (String, Long) -> Unit = { tag, time -> }
-    private var inited = false
+    private var global:DatabasePool? = null
 
-    fun init(login: String, pass: String, base: String, mysql_url: String, poolSize: Int, statisticCollector: (String, Long) -> Unit) {
-        Database.statisticCollector = statisticCollector
-        for (i in 0 until poolSize) {
-            pool.add(Item2(DatabaseInstance(login, pass, base, mysql_url), false))
-        }
-        inited = true
-    }
-
-    fun isInited() = inited
-
-    private fun getDatabase(): Item2<DatabaseInstance, Boolean> {
-        var x = 60000
-        while (x > 0){
-            val d = getDatabaseInLoop()
-            if(d != null) return d
-            ToolsThreads.sleep(1)
-            x--
-        }
-        throw RuntimeException("Can't get database instance")
-    }
-
-    private fun getDatabaseInLoop(): Item2<DatabaseInstance, Boolean>? {
-        var item:Item2<DatabaseInstance, Boolean>? = null
-        synchronized(pool) {
-            for (i in pool) if (!i.a2) {
-                i.a2 = true
-                item = i
-                break
-            }
-        }
-        return item
+    fun setGlobal(global:DatabasePool){
+        this.global = global
     }
 
     //
@@ -48,35 +13,11 @@ object Database {
     //
 
     fun insert(tag: String, query: SqlQueryInsert): Long {
-        val database = getDatabase()
-        val v: Long
-        try {
-            val t = System.currentTimeMillis()
-            v = database.a1.insert(query, query.requestValues.toTypedArray())
-            statisticCollector.invoke(tag, System.currentTimeMillis() - t)
-        } catch (e: Throwable) {
-            database.a2 = false
-            throw e
-        } finally {
-            database.a2 = false
-        }
-        return v
+        return global!!.insert(tag, query)
     }
 
     fun insert(tag: String, tableName: String, vararg o: Any?): Long {
-        val database = getDatabase()
-        val v: Long
-        try {
-            val t = System.currentTimeMillis()
-            v = database.a1.insert(tableName, *o)
-            statisticCollector.invoke(tag, System.currentTimeMillis() - t)
-        } catch (e: Throwable) {
-            database.a2 = false
-            throw e
-        } finally {
-            database.a2 = false
-        }
-        return v
+        return global!!.insert(tag, tableName, *o)
     }
 
     //
@@ -84,36 +25,11 @@ object Database {
     //
 
     fun select(tag: String, query: SqlQuerySelect): ResultRows {
-        val database = getDatabase()
-        val v: ResultRows
-        try {
-            val t = System.currentTimeMillis()
-            v = database.a1.select(query, query.requestValues.toTypedArray())
-            statisticCollector.invoke(tag, System.currentTimeMillis() - t)
-        } catch (e: Throwable) {
-            database.a2 = false
-            throw e
-        } finally {
-            database.a2 = false
-        }
-        return v
-
+        return global!!.select(tag, query)
     }
 
     fun select(tag: String, columnsCount: Int, query: String, vararg values: Any?): ResultRows {
-        val database = getDatabase()
-        val v: ResultRows
-        try {
-            val t = System.currentTimeMillis()
-            v = database.a1.select(columnsCount, query, *values)
-            statisticCollector.invoke(tag, System.currentTimeMillis() - t)
-        } catch (e: Throwable) {
-            database.a2 = false
-            throw e
-        } finally {
-            database.a2 = false
-        }
-        return v
+        return global!!.select(tag, columnsCount, query, *values)
     }
 
     //
@@ -121,19 +37,7 @@ object Database {
     //
 
     fun update(tag: String, query: SqlQueryUpdate): Int {
-        val database = getDatabase()
-        val v: Int
-        try {
-            val t = System.currentTimeMillis()
-            v = database.a1.update(query, query.requestValues.toTypedArray())
-            statisticCollector.invoke(tag, System.currentTimeMillis() - t)
-        } catch (e: Throwable) {
-            database.a2 = false
-            throw e
-        } finally {
-            database.a2 = false
-        }
-        return v
+        return global!!.update(tag, query)
     }
 
     //
@@ -141,17 +45,7 @@ object Database {
     //
 
     fun remove(tag: String, query: SqlQueryRemove) {
-        val database = getDatabase()
-        try {
-            val t = System.currentTimeMillis()
-            database.a1.remove(query, query.requestValues.toTypedArray())
-            statisticCollector.invoke(tag, System.currentTimeMillis() - t)
-        } catch (e: Throwable) {
-            database.a2 = false
-            throw e
-        } finally {
-            database.a2 = false
-        }
+        global!!.remove(tag, query)
     }
 
     //
@@ -159,16 +53,6 @@ object Database {
     //
 
     fun execute(tag: String, query: String?, vararg values: Any?) {
-        val database = getDatabase()
-        try {
-            val t = System.currentTimeMillis()
-            database.a1.execute(query, *values)
-            statisticCollector.invoke(tag, System.currentTimeMillis() - t)
-        } catch (e: Throwable) {
-            database.a2 = false
-            throw e
-        } finally {
-            database.a2 = false
-        }
+        global!!.execute(tag, query, *values)
     }
 }
